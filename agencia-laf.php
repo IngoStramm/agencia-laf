db86234e5d3f234eae7d115b4805e4924064cf40
Ingo Stramm <ingostramm@gmail.com>
2020-10-30 11:07:57 -0300

    include scripts

--- agencia-laf.php
+++ agencia-laf.php
@@ -8,7 +8,7 @@
  * Author URI:      https://agencialaf.com
  * Text Domain:     agencia-laf
  * Domain Path:     /languages
- * Version:         0.1.3
+ * Version:         0.1.4
  *
  * @package         Agencia_Laf
  */
@@ -37,6 +37,28 @@ function al_remove_link_dos_membros($title, $id = null)
     //     al_debug($title);
 
     return $title;
+}
+
+add_action('wp_enqueue_scripts', 'al_frontend_scripts');
+
+function al_frontend_scripts()
+{
+
+    if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '10.0.0.3'))) :
+        wp_enqueue_script('al-livereload', 'http://localhost:35729/livereload.js?snipver=1', array(), null, true);
+    endif;
+
+    $min = (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '10.0.0.3'))) ? '' : '.min';
+
+    // wp_enqueue_style( 'al-style', AL_URL . 'assets/css/style.css', array(), false, 'all' );
+
+    // wp_enqueue_script( 'jquery-mask', AL_URL . 'assets/lib/jquery.mask' . $min . '.js', array( 'jquery' ), '1.14.16', true );
+
+    wp_register_script('agencia-laf-script', AL_URL . 'assets/js/agencia-laf' . $min . '.js', array('jquery'), '1.0.1', true);
+
+    wp_enqueue_script('agencia-laf-script');
+
+    wp_localize_script('agencia-laf-script', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
 }
 
 require 'plugin-update-checker-4.10/plugin-update-checker.php';
