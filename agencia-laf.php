695c70523e06c5fc53d706aa63022b27ce2d1d85
Ingo Stramm <ingostramm@gmail.com>
2020-10-30 10:14:16 -0300

    first commit

---
+++ agencia-laf.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Plugin Name:     Agência LAF
+ * Plugin URI:      https://agencialaf.com
+ * Description:     Este plugin é parte integrante do site da Agência LAF
+ * Author:          Ingo Stramm
+ * Author URI:      https://agencialaf.com
+ * Text Domain:     agencia-laf
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Agencia_Laf
+ */
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+define('AL_DIR', plugin_dir_path(__FILE__));
+define('AL_URL', plugin_dir_url(__FILE__));
+
+function al_debug($debug)
+{
+    echo '<pre>';
+    var_dump($debug);
+    echo '</pre>';
+}
+
+add_filter('the_title', 'al_remove_link_dos_membros', 10, 2);
+
+function al_remove_link_dos_membros($title, $id = null)
+{
+    if (!$id || is_admin()) return $title;
+
+    $post_type = get_post_type($id);
+    if($post_type == 'digeco_team')
+        al_debug($title);
+
+    return $title;
+}
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Agência LAF ===
+Requires at least: 4.7
+Tested up to: 5.5
+Stable tag: 4.3
+Requires PHP: 7.1
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Este plugin é parte integrante do site da Agência LAF.
+
+== Description ==
+
+Este plugin é parte integrante do site da Agência LAF.
