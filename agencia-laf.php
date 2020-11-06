fa31da352aa10ecea04c1b80fbf8ec0cfd655374
Ingo Stramm <ingostramm@gmail.com>
2020-11-06 09:20:01 -0300

    remove o href dos links da equipe

--- agencia-laf.php
+++ agencia-laf.php
@@ -8,7 +8,7 @@
  * Author URI:      https://agencialaf.com
  * Text Domain:     agencia-laf
  * Domain Path:     /languages
- * Version:         0.1.9
+ * Version:         0.1.10
  *
  * @package         Agencia_Laf
  */
--- assets/js/agencia-laf.js
+++ assets/js/agencia-laf.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('.al-team-grid .rtin-title a');
     Array.prototype.forEach.call(links, function (el, i) {
+        el.setAttribute('href', '');
         el.addEventListener('click', function (ev) {
             ev.preventDefault();
         });
